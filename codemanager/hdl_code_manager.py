d81531caaaadd5ad97687b795d06be6c2f57e46d
Marwin Kirchhofs <m.kirchhofs@gmx.de>
2024-03-24 15:59:34 +0100

    starting hdl code manager implementation - systemverilog and hdl classes, systemverilog modules, hdl project generation project directory handling

--- codemanager/hdl_code_manager.py
+++ codemanager/hdl_code_manager.py
@@ -6,6 +6,7 @@
 
 import os, re
 import code_manager
+from operator import itemgetter
 
 LANG_IDENTIFIERS = ["hdl"]
 
@@ -17,8 +18,76 @@ class HdlCodeManager(code_manager.CodeManager):
         # extensive) comment in python_code_manager
         super().__init__("hdl")
 
+        # set variables for hdl project directory structure
+        self.PRJ_DIRS = {                                   \
+                'rtl':          "rtl",                      \
+                'simulation':   "sim",                      \
+                'testbench':    "tb",                       \
+                'constraints':  "constraints",              \
+                'tcl':          "tcl",                      \
+                'blockdesign':  "bd",                       \
+                'xilinx_ips':   "xips",                     \
+                'software':     "sw",                       \
+        }
 
     def _command_project(self, specifier, **args):
+        """Creates the skeleton for an hdl project as generic as possible. That 
+        mainly is, create the hdl project directory structure and add common 
+        build scripts like makefile and vivado project generation script (given 
+        that a xilinx project is asked for)
 
-        # TODO: placeholder
-        print(f"creating a project using {self.TEMPLATES_ABS_PATH}/template_project")
+        !!! The method can create a new project, including project directory, or 
+        act from within an existing project directory. This is decided on 
+        whether or not args["target"] is not specified (which means passing a -t 
+        option to m_code_manager or not). !!!
+
+        An existing project will never be deleted. If the user confirms to 
+        edit/overwrite an existing directory, that means that the contents will 
+        be added to the existing directory (instead of doing nothing). The 
+        design guideline is to really only delete files when that is 
+        unambiguously confirmed. In this case, if the user really wants an 
+        entirely new project, they can easily delete an existing one manually.
+        """
+
+        # TODO: only directory creation so far to facilitate developing other 
+        # command handlers. IMPLEMENT THE REST!
+
+        # TODO: temporary rtl directory structure. So far, everything gets 
+        # dumped into 'rtl' with no subdirectories whatsoever. Works for small 
+        # projects, not ideal for larger projects, but that's something to 
+        # address later on.
+
+        if "target" in args:
+            prj_name = args["target"]
+            if self._check_target_edit_allowed(prj_name):
+                try:
+                    os.mkdir(prj_name)
+                except:
+                    # no need to handle the exception if the directory prj_name 
+                    # exists, that's taken care of and confirmed in 
+                    # self._check_target_edit_allowed
+                    pass
+                os.chdir(prj_name)
+
+        # some directories are omitted here: xips, bd
+        # reason: they really only makes sense when the respective feature is 
+        # used, and it's less likely/unintended that the user is going to edit 
+        # or create things in these directories without going through the 
+        # codemanager flow. Let's see how many days it takes until I get proven 
+        # wrong about the last sentence (today is the 2024-03-24)...
+        project_dirs = itemgetter(
+                'rtl', 'constraints', 'simulation', 'testbench', 'tcl')(self.PRJ_DIRS)
+        for directory in project_dirs:
+            if self._check_target_edit_allowed(directory):
+                try:
+                    os.mkdir(directory)
+                except:
+                    # again, if a project directory already exists, that's fine 
+                    # (assuming that it's a directory, theoretically could be 
+                    # a file as well. but at some point users gotta act 
+                    # reasonably, such as not to create files with meaningful 
+                    # names and without file extensions)
+                    pass
+
+
+
--- codemanager/systemverilog_code_manager.py
+++ codemanager/systemverilog_code_manager.py
@@ -16,13 +16,41 @@ class SystemverilogCodeManager(code_manager.CodeManager):
         # why passing the language to the base class init? See (way too 
         # extensive) comment in python_code_manager
         super().__init__("systemverilog")
+#       Why the additional hdl code manager?
+#       It's handy if generic hdl stuff is also accessible with sv/systemverilog 
+#       as the language specifier. So normally you would just inherit 
+#       systemverilog_code_manager from hdl_code_manager, and there you are.  
+#       Problem: If the hdl codemanager methods need templates, those would be 
+#       in the hdl templates directory.  But self.TEMPLATES_ABS_PATH would point 
+#       to systemverilog. Best I could come up with is the additional internal 
+#       hdl code manager with a pass-through of hdl code  manager commands (see 
+#       run_code_manager_command). Creating soft links from the systemverilog 
+#       directory into the hdl directory is against all rules of portability.
         self.hdl_code_manager = hdl_code_manager.HdlCodeManager()
 
 
     def _command_module(self, specifier, **args):
 
-        # TODO: placeholder
-        print(f"creating a module using {self.TEMPLATES_ABS_PATH}/template_project")
+        # check if module directory exists. if it doesn't, abort straight-away?
+        if not os.path.isdir(self.hdl_code_manager.PRJ_DIRS['rtl']):
+            print(f"Project rtl directory '{self.hdl_code_manager.PRJ_DIRS['rtl']}' " \
+            "could not be found found. A potential reason is that you are not in " \
+            "the project top-level directory. No file will be touched")
+            return
+
+        s_module = args["target"]
+        s_target_file = os.path.join(
+                self.hdl_code_manager.PRJ_DIRS['rtl'], s_module + ".sv")
+        template_out = self._load_template("module", {"MODULE": s_module})
+        self._write_template(template_out, s_target_file)
+
+
+    def _command_inst(self, specifier, **args):
+        print(f"creating an instantiation of systemverilog module f{args['target']}")
+
+
+    def _command_testbench(self, specifier, **args):
+        print(f"creating a testbench for systemverilog module f{args['target']}")
 
 
     def run_code_manager_command(self, command, specifier, **args):
@@ -36,5 +64,4 @@ class SystemverilogCodeManager(code_manager.CodeManager):
         except:
             fun_command = getattr(self.hdl_code_manager, '_command_' + command)
         fun_command(specifier, **args)
-#         print("Please implement this function for each language-specific Code_Manager!")
         
