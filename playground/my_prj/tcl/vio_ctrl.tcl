42f46537bb6bb0c951d767a86d511c7d025f6632
Marwin Kirchhofs <m.kirchhofs@gmx.de>
2024-04-05 11:04:02 +0200

    temporary vio_ctrl.tcl file cleaned up (before converting into a template)

--- playground/my_prj/tcl/vio_ctrl.tcl
+++ playground/my_prj/tcl/vio_ctrl.tcl
@@ -1,51 +1,20 @@
 
 # TOP LEVEL VIO CONTROL
 # connects to a top level xilinx vio core, which acts as more of a 'prototyping' 
-# hardware control interface solution that a memory mapped axi interface.
+# hardware control interface solution than a memory mapped axi interface.
 #
-# how to add a new port:
-# * add port to vio_ports_write/read
-# * if it's a multi-bit port: set port radix in mcm_vio_init
-# * add getter/setter
-# * optional:
-#     * set up a dict_* for the port values
-#     * add the port's getter to mcm_vio_print_status
-#
+# quick usage:
+# after sourcing the script, make sure to run mcm_vio_init. That opens the 
+# hardware manager, detects the vio core, connects to it and sets up the vio 
+# object. It also links the probes file to the vio and sets up the user-defined 
+# radices for interacting with vio signals (soon).
+# After that, you can operate any signal by using the mcm_vio_read/write 
+# functions. Everything else on top of that, like resetting and printing the 
+# system status, is built on top of that.
 #
 
 package require json
 
-set dir_hw_export hw_export
-
-set vio_ctrl_signals [::json::json2dict [read [open vio_ctrl_signals.json r]]]
-set vio_ltx_data [::json::json2dict [read [open [glob hw_export/latest/*.ltx] r]]]
-
-# general comment about the many times that we have interchanging 'lindex' and 
-# 'dict get' here: I don't know where it went wrong, if the ltx format is not 
-# "exactly" json, or if there is a reason for multiple one-element lists. As of 
-# now (2024-04) the code is tested with vivado 2019.1. It might as well break 
-# with different vivado versions.
-# (here, vio_ltx_data[0] only says "ltx_root", vio_ltx_data[1] is where the 
-# information is
-set debug_cores [dict get [lindex [dict get [lindex $vio_ltx_data 1] ltx_data] 0] debug_cores]
-
-# find the element in debug_cores that describes the vio_ctrl instance. That is 
-# exactly the one whose name is "inst_xip_vio_ctrl". Theoretically, you could 
-# use 'lmap' here to filter the list. However, at least for Vivado 2019.1 the 
-# tcl version (8.5) is too old for that (>=8.6), so we go the conservative 
-# iterate and break way
-set d_debug_core_vio_ctrl ""
-foreach core $debug_cores {
-    if {[dict get $core "name"] eq "inst_xip_vio_ctrl"} {
-        set d_debug_core_vio_ctrl $core
-        break
-    }
-}
-
-set vio_port_direction_identifiers [dict create                                 \
-    in                              "INPUT_VALUE"                               \
-    out                             "OUTPUT_VALUE"                              \
-]
 
 
 proc _vio_ctrl_get_port_direction {user_signal_name} {
@@ -96,10 +65,6 @@ proc mcm_vio_read {port_name} {
 
     set port_name_vio_ctrl [_vio_ctrl_get_port_name $port_name]
     set port_direction [_vio_ctrl_get_port_direction $port_name]
-    # Reading from the vio is done via querying a property. Depending on if it 
-    # is an input or an output to the vio ip, that property is either named 
-    # 'INPUT_VALUE' or 'OUTPUT_VALUE'. The following line retrieves the correct 
-    # property name.
     set port_direction_specifier [dict get $vio_port_direction_identifiers $port_direction]
 
     if {$port_direction eq "in"} {
@@ -128,27 +93,59 @@ proc mcm_vio_write {port_name value} {
 }
 
 ############################################################
-# PARAMETERS
+# SCRIPT SETUP
 ############################################################
 
-##############################
-# SYSTEM
-##############################
-
-# set vio_ports_write [dict create                                                \
-#     sys_reset                       "probe_out0"                                \
-#     counter                         "counter"                                   \
-# ]
-# set vio_ports_read [dict create                                                 \
-#     switches                        "switches"                                  \
-# ]
-
+set dir_hw_export hw_export
 set name_vio_gt_interface inst_xip_vio_ctrl
 # TODO: find a generic way to determine the hw device, or to set it in the 
 # project config
 set name_hw_device xc7a35t_0
 
+# parse vio signal description json file and vivado-generated debug probes file 
+# (which in fact is also json)
+set vio_ctrl_signals [::json::json2dict [read [open vio_ctrl_signals.json r]]]
+set vio_ltx_data [::json::json2dict [read [open [glob hw_export/latest/*.ltx] r]]]
+
+# general comment about the many times that we have interchanging 'lindex' and 
+# 'dict get' in this script: I don't know where it went wrong, if the ltx format 
+# is not "exactly" json, or if there is a reason for multiple one-element lists.  
+# As of now (2024-04) the code is tested with vivado 2019.1. It might as well 
+# break with different vivado versions.
+# (here, vio_ltx_data[0] only says "ltx_root", vio_ltx_data[1] is where the 
+# information is
+set debug_cores [dict get [lindex [dict get [lindex $vio_ltx_data 1] ltx_data] 0] debug_cores]
+
+# find the element in debug_cores that describes the vio_ctrl instance. That is 
+# exactly the one whose name is "inst_xip_vio_ctrl". Theoretically, you could 
+# use 'lmap' here to filter the list. However, at least for Vivado 2019.1 the 
+# tcl version (8.5) is too old for that (>=8.6), so we go the conservative 
+# iterate and break way
+set d_debug_core_vio_ctrl ""
+foreach core $debug_cores {
+    if {[dict get $core "name"] eq "inst_xip_vio_ctrl"} {
+        set d_debug_core_vio_ctrl $core
+        break
+    }
+}
+
+# Reading from the vio is done via querying a property. Depending on if it is an 
+# input or an output to the vio ip, that property is either named 'INPUT_VALUE' 
+# or 'OUTPUT_VALUE'. Hence this dictionary, just for convenience in converting 
+# that.
+set vio_port_direction_identifiers [dict create                                 \
+    in                              "INPUT_VALUE"                               \
+    out                             "OUTPUT_VALUE"                              \
+]
+
+
+##############################
+# SYSTEM
+##############################
+
+
 # LOOPBACK CONSTANTS
+# TODO: support these kind of dicts in the getter/setter methods
 set dict_switches_status [dict create                                           \
     "1000"                          0                                           \
     "0100"                          1                                           \
@@ -156,13 +153,9 @@ set dict_switches_status [dict create                                           
     "0001"                          3                                           \
 ]
 
-##############################
-# PROJECT
-##############################
-
 
 ############################################################
-# INITIALIZATION
+# VIO INITIALIZATION
 ############################################################
 
 # get the latest *.ltx file that has been exported (via manage_hw.bash) to
@@ -213,48 +206,6 @@ proc mcm_vio_detect {} {
                 -filter CELL_NAME=~$name_vio_gt_interface]
 }
 
-# # write VIO output port
-# proc _mcm_vio_write {probe_name val} {
-#     global vio_gt_interface
-#     global vio_ports_write
-# 
-#     startgroup
-#     # assign output value to port
-#     set_property OUTPUT_VALUE $val [                                            \
-#                 get_hw_probes [dict get $vio_ports_write $probe_name]           \
-#                 -of_objects $vio_gt_interface]
-#     # commit port to hardware
-#     commit_hw_vio [                                                             \
-#                 get_hw_probes [dict get $vio_ports_write $probe_name]           \
-#                 -of_objects $vio_gt_interface]
-#     endgroup
-# }
-
-# # read VIO port
-# # Defaults to reading an input port. For getting an output port's value, specify
-# # output_port=1
-# # (depending on the type of port, you either need the INPUT_VALUE or the
-# # OUTPUT_VALUE property, hence the distinction in the function argument)
-# proc _mcm_vio_read {probe_name {output_port 0}} {
-#     global vio_gt_interface
-#     global vio_ports_read
-#     global vio_ports_write
-# 
-#     # TODO: couldn't you just get rid of the read/write distinction, if you just 
-#     # always update the vio, and then look up both dictionaries? Then you just 
-#     # have to impose no double signal names, should be common sense at least.
-#     if {$output_port eq 0} {
-#         refresh_hw_vio $vio_gt_interface
-#         set read_val [get_property INPUT_VALUE [                                \
-#                     get_hw_probes [dict get $vio_ports_read $probe_name]        \
-#                     -of_objects $vio_gt_interface]]
-#     } else {
-#         set read_val [get_property OUTPUT_VALUE [                               \
-#                     get_hw_probes [dict get $vio_ports_write $probe_name]       \
-#                     -of_objects $vio_gt_interface]]
-#     }
-#     return $read_val
-# }
 
 # initialize the VIO connection
 # * set up probes file (the newest one that is exported, if none passed)
@@ -296,40 +247,36 @@ proc mcm_vio_init {{probes_file ""}} {
 # HIGH-LEVEL FUNCTIONALITY
 ##############################
 
+# --- EXAMPLE ---
 proc mcm_vio_print_status {} {
     # GATHER STATUS
-    set switches [mcm_vio_get switches]
+    set sig [mcm_vio_get sig]
     set counter [mcm_vio_get counter]
 
     # PRINT STATUS
     puts "-------------------------------------------------------------"
     puts "SYSTEM STATUS"
-    # counter
-    puts "COUNTER:        $counter"
-    # switches
-    puts "SWITCHES:       switches"
+    # <signal>
+    puts "SIGNAL:         $signal"
     puts "-------------------------------------------------------------"
 }
 
-# example: perform an operation
+# --- EXAMPLE ---
 proc mcm_vio_perform_operation {} {
 
     # trigger measurement
-    mcm_vio_set_counter 1
+    mcm_vio_set sig 1
 
     # wait for hardware processing (10ms)
     after 10
 
     # check status
-    set status [mcm_vio_get_switches]
+    set status [mcm_vio_get sig]
 
-    puts "switch position: $status"
+    puts "signal value: $status"
 }
 
-##############################
-# DEFAULTS
-##############################
-
+# --- EXAMPLE ---
 # initialize system to operation mode defaults
 proc mcm_vio_set_defaults {{mode ""}} {
 
@@ -342,6 +289,7 @@ proc mcm_vio_set_defaults {{mode ""}} {
             # -------------------- #
             # (mode dependent default values here)
             # -------------------- #
+            mcm_vio_set sig 0
         }
         default {
             puts "Please specify one of the supported modes:"
